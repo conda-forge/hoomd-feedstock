0d53ddbdbc0d3d2cdec2b66097eba9ab87ecaddd
Bradley Dice <bdice@bradleydice.com>
2020-04-28 15:02:33 -0400

    Add CUDA support.

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,11 +14,16 @@ else
     export LINUX_ADDITIONAL='-DDL_LIB= -DUTIL_LIB='
 fi
 
+CUDA_SUPPORT="off"
+if [[ $1 == "gpu" ]]; then
+    CUDA_SUPPORT="on"
+fi
+
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
       -DENABLE_MPI=off \
-      -DENABLE_CUDA=off \
+      -DENABLE_CUDA=${CUDA_SUPPORT} \
       -DBUILD_TESTING=on \
       -DENABLE_TBB=on \
       -DBUILD_JIT=off \
