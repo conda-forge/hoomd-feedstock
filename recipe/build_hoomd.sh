49bc8cb68adfe2292d7d9c6ba948b9d679c0a3a1
Joshua A. Anderson <joaander@umich.edu>
2021-03-23 13:04:19 -0400

    Try CMAKE_ARGS again

--- recipe/build_hoomd.sh
+++ recipe/build_hoomd.sh
@@ -2,13 +2,9 @@ mkdir -p build-conda
 cd build-conda
 rm -rf ./*
 
-export CPATH=${PREFIX}/include
-export CMAKE_PREFIX_PATH=${PREFIX}
-export TBB_LINK=${PREFIX}/lib
-
 if [ "$(uname)" == "Darwin" ]; then
     # temporary hack. Remove when upstream supports the new pybind11 mode: https://pybind11.readthedocs.io/en/stable/cmake/index.html#new-findpython-mode
-   export ADDITIONAL_OPTIONS="-DPYBIND11_FINDPYTHON=ON -DPython_ROOT_DIR=${PREFIX}"
+   export ADDITIONAL_OPTIONS="-DPYBIND11_FINDPYTHON=ON"
 else
     export ADDITIONAL_OPTIONS='-DDL_LIB= -DUTIL_LIB='
 fi
@@ -22,7 +18,11 @@ if [[ $1 == "gpu" ]]; then
     export CXXFLAGS="$(echo $CXXFLAGS | sed -e 's/ -std=[^ ]*//')"
 fi
 
+echo CMake Args: ${CMAKE_ARGS}
+
 cmake ../ \
+      ${CMAKE_ARGS} \
+      -Dlibgetar_DIR=../hoomd/extern/libgetar \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
       -DENABLE_MPI=off \
