2476da91b09a854f6143b4f6220940e547f90436
Bradley Dice <bdice@bradleydice.com>
2023-06-28 13:05:37 -0500

    CUDA 12 build working.

--- recipe/build_hoomd.sh
+++ recipe/build_hoomd.sh
@@ -2,11 +2,27 @@ mkdir -p build-conda
 cd build-conda
 rm -rf ./*
 
+env
+
 CUDA_SUPPORT="off"
 CUDA_CMAKE_OPTIONS=""
 if [[ $1 == "gpu" ]]; then
     CUDA_SUPPORT="on"
-    CUDA_CMAKE_OPTIONS="-DCMAKE_CUDA_COMPILER=${CUDA_HOME}/bin/nvcc -DCMAKE_CUDA_HOST_COMPILER=${CXX}"
+
+    if [[ "${cuda_compiler_version}" = 11* ]]; then
+        CUDA_CMAKE_OPTIONS="-DCMAKE_CUDA_COMPILER=${CUDA_HOME}/bin/nvcc -DCMAKE_CUDA_HOST_COMPILER=${CXX}"
+    else
+        CUDA_CMAKE_OPTIONS="-DCMAKE_CUDA_HOST_COMPILER=${CXX}"
+
+        [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
+        [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
+        [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
+
+        # The conda-forge build system does not provide libcuda from an NVIDIA
+        # driver, so we link to the stub.
+        CUDA_CMAKE_OPTIONS="${CUDA_CMAKE_OPTIONS} -DCUDA_cuda_LIBRARY=${PREFIX}/${targetsDir}/lib/stubs/libcuda.so"
+    fi
+
     # remove -std=c++17 from CXXFLAGS for compatibility with nvcc
     export CXXFLAGS="$(echo $CXXFLAGS | sed -e 's/ -std=[^ ]*//')"
 fi
