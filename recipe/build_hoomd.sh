8b7d951d0d6f3488aab76b10e2df7c09371aeff8
Joshua A. Anderson <joaander@umich.edu>
2026-09-16 09:57:55 -0400

    libpython is required as a dependency
    https://conda-forge.zulipchat.com/#narrow/channel/457337-general/topic/Rebuild.20for.20python.203.2E15.20.28gst-python.29.20migration.20failure/with/624654312

--- recipe/build_hoomd.sh
+++ recipe/build_hoomd.sh
@@ -1,3 +1,5 @@
+set -x
+
 mkdir -p build-conda
 cd build-conda
 rm -rf ./*
@@ -28,7 +30,6 @@ cmake ../ \
       -DENABLE_GPU=${CUDA_SUPPORT} ${CUDA_CMAKE_OPTIONS} \
       -DBUILD_TESTING=off \
       -DPLUGINS="" \
-      -DPYTHON_Executable=$PYTHON \
       -DPYTHON_SITE_INSTALL_DIR="lib/python${PY_VER}/site-packages/hoomd" \
       -GNinja
 
